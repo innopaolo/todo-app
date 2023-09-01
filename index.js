51ecf31573046f1e6c4f6971d36371ac9ba974b5
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-09-02 01:23:28 +0800

    Create modal that will add more properties to each list object

--- index.js
+++ index.js
@@ -22,20 +22,28 @@
         input.value = "";
     });
 
-    ul.addEventListener("click", e => {
-        let id = e.target.getAttribute("data-id");
-
-        // No response if blank area clicked
-        if (!id) return
-        
-        removeFromContainer(id);
-        removeFromArray(id);
-    });
+ 
 
     function appendToContainer(todoTask, itemId) {
         const li = document.createElement("li");
-        li.textContent = todoTask;
+        const div = document.createElement("div")
+
+        // Create icon buttons for deleting and importance setting
+        div.classList.add("icon-buttons");
+        div.innerHTML = "<img src='flag-solid.svg'><img src='trash-can-solid.svg'>";
+
+        // Create a separate element for the task text
+        const taskText = document.createElement("span");
+        taskText.textContent = todoTask;
+
+        // Append the div with the task text and icons to the li
+        li.appendChild(taskText);
+        li.appendChild(div);
+        
+        // Give both span element and its parent li an ID 
         li.setAttribute("data-id", itemId);
+        taskText.setAttribute("data-id", itemId);
+
         ul.appendChild(li);
     }
 
@@ -60,9 +68,13 @@
     const themeBtn = document.getElementById("theme-btn");
     const container = document.querySelector(".container");
     const formInput = document.querySelector(".form-input");
+    const modalContent = document.querySelector(".modal-content");
+    const modalImage = document.getElementById("modal-image");
+    const modalTxtarea = document.getElementById("description");
+
 
     themeBtn.addEventListener("click", () => {
-        
+
         // Default class "fa-moon" switches to "fa-sun" on toggle
         themeBtn.classList.toggle("fa-sun");
 
@@ -74,6 +86,11 @@
             document.body.classList.add("changeThemeBody");
             container.classList.add("changeThemeContainer");
             formInput.classList.add("changeThemeInput");
+            modalContent.classList.add("changeThemeModal");
+            modalTxtarea.classList.add("changeThemeInput");
+
+            // Change to white version of the ship's wheel button
+            modalImage.src = "ship-wheel-light.png";
 
         } else {
 
@@ -83,6 +100,59 @@
             document.body.classList.remove("changeThemeBody");
             container.classList.remove("changeThemeContainer");
             formInput.classList.remove("changeThemeInput");
+            modalContent.classList.remove("changeThemeModal");
+            modalTxtarea.classList.remove("changeThemeInput");
+
+            // Change to black version of the ship's wheel button
+            modalImage.src = "ship-wheel.png";
         }
     });
+
+
+    // Get the modal element
+    const modal = document.getElementById("myModal");
+
+    // Get the close button element inside the modal
+    const closeBtn = document.querySelector(".close");
+
+    // Function to open the modal
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    // Function to close the modal
+    function closeModal() {
+        modal.style.display = "none";
+    }
+
+    // Event Listener for opening the modal
+    ul.addEventListener("click", e => {
+        
+        let id = e.target.getAttribute("data-id");
+        // removeFromContainer(id);
+        // removeFromArray(id);
+
+        // No response if blank area clicked
+        if (!id) return
+        
+        
+        if (e.target.tagName === "SPAN") {
+            openModal();
+        }
+      
+    });
+
+    // Event listener for closing the modal when clicking the close button
+    closeBtn.addEventListener("click", closeModal);
+
+    // Event listener for closing the modal when clicking outside the modal content
+    window.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+
+    
+
 })();
